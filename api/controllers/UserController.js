5fbebb9b25ec0662805672555f949c472b97e70f
anpandu <anpandumail@gmail.com>
2016-02-09 23:35:14 +0700

    User: login beta

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,21 @@
  */
 
 module.exports = {
-	
+
+  login: function(req, res) {
+    var id = req.param('user_id')
+    User
+      .findOne({ user_id: id })
+      .then(function(user) {
+        var token = user.user_id
+        return res.send({
+          token: token
+        })
+      })
+      .catch(function () {
+        return res.notFound()
+      })
+  }
+
 }
 
--- tests/controllers/user.ctrl.test.js
+++ tests/controllers/user.ctrl.test.js
@@ -11,7 +11,7 @@ describe('UserController', function() {
       Promise.resolve()
         .then(function () { 
           return User.create({ 
-            user_id:'123',  
+            user_id:'user_1',  
             user_name:'123',  
             display_name:'123',  
             password:'123',  
@@ -25,7 +25,7 @@ describe('UserController', function() {
             .post(endpoint)
             .set('Content-Type', 'application/json')
             .send({ 
-              user_id:'123',  
+              user_id:'user_1',  
               user_name:'123',  
               display_name:'123',  
               password:'123',  
@@ -41,6 +41,56 @@ describe('UserController', function() {
               assert('email' in user, 'email field doesn\'t exist' )
               assert('avatar' in user, 'avatar field doesn\'t exist' )
               assert(user.user_id == _user.user_id, 'wrong user' )
+            })
+            .end(done)
+        })
+    })
+
+  })
+
+  describe('/login', function() {
+
+    it('should return token if user exist', function (done) {
+      var _user
+      Promise.resolve()
+        .then(function () { 
+          return User.create({ 
+            user_id:'user_2',  
+            user_name:'123',  
+            display_name:'123',  
+            password:'123',  
+            email:'123',  
+            avatar:'123',  
+          }) 
+        })
+        .then(function (user) {
+          _user = user
+          request(sails.hooks.http.app)
+            .post(endpoint+'/login')
+            .set('Content-Type', 'application/json')
+            .send({ 
+              user_id: 'user_2'
+            })
+            .expect(function(res) {
+              var result = res.body
+              assert(user.user_id == result.token, 'wrong user' )
+            })
+            .end(done)
+        })
+    })
+
+    it('should return 404 if user doesn\'t exist', function (done) {
+      var _user
+      Promise.resolve()
+        .then(function () {
+          request(sails.hooks.http.app)
+            .post(endpoint+'/login')
+            .set('Content-Type', 'application/json')
+            .send({ 
+              user_id: 'user_3'
+            })
+            .expect(function(res) {
+              assert(res.status == '404', 'not 404' )
             })
             .end(done)
         })
