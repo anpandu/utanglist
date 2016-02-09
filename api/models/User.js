41f3759e3f46d856cb77853a52e1ba323bbf19ec
anpandu <anpandumail@gmail.com>
2016-02-09 21:10:17 +0700

    User

---
+++ api/models/User.js
@@ -0,0 +1,35 @@
+/**
+* User.js
+*
+* @description :: TODO: You might write a short summary of how this model works and what it represents here.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+    
+    id: {
+      type: 'string',
+      required: true,
+    },
+    user_name: {
+      type: 'string'
+    },
+    display_name: {
+      type: 'string'
+    },
+    password: {
+      type: 'string'
+    },
+    email: {
+      type: 'string'
+    },
+    avatar: {
+      type: 'string'
+    },
+
+  }
+
+}
+
