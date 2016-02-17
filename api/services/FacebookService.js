82847a66848c8f5dfae04d2039fa82620d846d2e
anpandu <anpandumail@gmail.com>
2016-02-18 00:34:54 +0700

    FacebookService getUserBio

--- api/services/FacebookService.js
+++ api/services/FacebookService.js
@@ -3,7 +3,24 @@ module.exports = {
 
   basic: function () {
     return 'FacebookService'
-  }
+  },
+
+  getUserBio: function (user_id, token) {
+    return new Promise (function (resolve, reject) {
+      graph
+        .setAccessToken(token)
+        .get('/me?fields=id,name,picture,first_name,last_name', function(err, res) {
+          var bio = {
+            id: res.id,
+            full_name: res.name,
+            avatar: res.picture.data.url,
+            first_name: res.first_name,
+            last_name: res.last_name,
+          }
+          resolve(bio)
+        })
+    })
+  },
 
 }
 
